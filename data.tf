1fb74550745104b610f3cd3e74b9b1fa976e22f5
armdupre <a-joseph.dupre@keysight.com>
2024-01-16 22:26:06 -0500

    Updated.

--- data.tf
+++ data.tf
@@ -5,7 +5,7 @@ data "azurerm_shared_image_gallery" "SharedImageGallery" {
 
 data "azurerm_shared_image" "SharedImageVersion" {
 	name = local.ImageVersion
-	name = local.ImageSku
+	image_name = local.ImageSku
 	gallery_name = local.SharedImageGalleryName
 	resource_group_name = local.SharedImageGalleryResourceGroupName
 }
